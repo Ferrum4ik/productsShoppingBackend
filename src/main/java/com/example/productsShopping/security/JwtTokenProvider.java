ee4b51703fbda395ac38ca2846a49e585d0108c1
Fazil Mamedov <fazilnazile@gmail.com>
2025-04-13 11:00:12 +0400

    add jwt security

---
+++ src/main/java/com/example/productsShopping/security/JwtTokenProvider.java
@@ -0,0 +1,71 @@
+package com.example.productsShopping.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    @Value("${jwt_secret}")
+    private String jwtSecret;
+
+    @Value("${jwt_expiration}")
+    private int jwtExpirationInMs;
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
+    public String generateToken(Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    // Извлечение имени пользователя из токена
+    public String getUsernameFromJWT(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(authToken);
+            return true; // Токен валиден
+        } catch (SecurityException ex) {
+            // Неверная подпись токена
+        } catch (MalformedJwtException ex) {
+            // Неверный формат токена
+        } catch (ExpiredJwtException ex) {
+            // Токен истек
+        } catch (UnsupportedJwtException ex) {
+            // Токен не поддерживается
+        } catch (IllegalArgumentException ex) {
+            // Пустая строка токена
+        }
+        return false; // Токен не валиден
+    }
+
+}
