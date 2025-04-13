0f21d54125b9f87a461506b2bb9fc3a7f07749e6
Fazil Mamedov <fazilnazile@gmail.com>
2025-04-13 11:14:11 +0400

    add Custom User Details Service

--- src/main/java/com/example/productsShopping/repository/UserRepository.java
+++ src/main/java/com/example/productsShopping/repository/UserRepository.java
@@ -4,8 +4,13 @@ import com.example.productsShopping.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+
+
 }
